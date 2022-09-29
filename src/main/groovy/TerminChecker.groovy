ff44347bd75b1d9b0dcaa7fd2b9ecf299caf714d
Youssef.Nassar <yossef41997@gmail.com>
2022-09-29 12:28:50 +0200

    fix changed elements - add waiting  time - make notification sound OS independent

--- src/main/groovy/TerminChecker.groovy
+++ src/main/groovy/TerminChecker.groovy
@@ -1,8 +1,7 @@
 import geb.Browser
 import geb.driver.CachingDriverFactory
+import javazoom.jl.player.Player
 import org.openqa.selenium.By
-
-import java.awt.Toolkit
 
 class TerminChecker {
     void checkTermin() {
@@ -10,50 +9,50 @@ class TerminChecker {
             Browser.drive {
                 browser.go("https://otv.verwalt-berlin.de/")
                 driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/form/div/div/div/div/div/div/div/div/div/div[1]/div[1]/div[2]/a")).click()
-                Thread.sleep(5000);
+                Thread.sleep(8000)
                 driver.findElement(By.id("xi-cb-1")).click()
                 driver.findElement(By.id("applicationForm:managedForm:proceed")).click()
-                Thread.sleep(6000);
+                Thread.sleep(10000)
                 // stastsangehörigkeit dropdown
                 driver.findElement(By.id("xi-sel-400")).click()
-                Thread.sleep(2000);
+                Thread.sleep(6000)
                 // Syrien
-                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[2]/select/option[160]")).click()
+                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[1]/select/option[160]")).click()
                 //Anzahl der Personen drop down
                 driver.findElement(By.id("xi-sel-422")).click()
                 // eine Person
-                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[3]/div[1]/div[1]/select/option[2]")).click()
-                Thread.sleep(4000)
+                driver.findElement(By.id("xi-sel-422_1")).click()
+                Thread.sleep(8000)
                 //Leben sie in Berlin mit einem Familienangehörigen
                 driver.findElement(By.id("xi-sel-427")).click()
                 //nein
-                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[4]/select/option[3]")).click()
-                Thread.sleep(2000)
+                driver.findElement(By.id("xi-sel-427_2")).click()
+                Thread.sleep(8000)
                 //Aufentahltstitel beantragen
-                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[9]/div[1]/div[1]/div[1]/div[1]/label")).click()
-                Thread.sleep(1000)
+                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[8]/div[1]/div[1]/div[1]/div[1]/label")).click()
+                Thread.sleep(3000)
                 //Erwerbstätigkeit
-                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[9]/div[1]/div[1]/div[1]/div[5]/div/div[3]")).click()
-                Thread.sleep(1000)
+                driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[4]/div[2]/form/div[2]/div/div[2]/div[8]/div[2]/div[2]/div[1]/fieldset/div[8]/div[1]/div[1]/div[1]/div[5]/div/div[3]/label")).click()
+                Thread.sleep(3000)
                 //Aufenthaltserlaubnis für Fachkräfte zur Arbeitsplatzsuche - Erteilung (§ 20)
                 driver.findElement(By.id("SERVICEWAHL_DE473-0-1-1-324661")).click()
-                Thread.sleep(4000)
-                driver.findElement(By.id("applicationForm:managedForm:proceed")).click();
-                Thread.sleep(5000);
+                Thread.sleep(7000)
+                driver.findElement(By.id("applicationForm:managedForm:proceed")).click()
+                Thread.sleep(8000)
 
-                boolean available = false;
+                boolean available = false
                 while (!available) {
                     if (driver.findElement(By.id("messagesBox")).getText().
                             contains("Für die gewählte Dienstleistung sind aktuell keine Termine frei! Bitte")) {
-                        Thread.sleep(6000);
-                        driver.findElement(By.id("applicationForm:managedForm:proceed")).click();
+                        Thread.sleep(8000)
+                        driver.findElement(By.id("applicationForm:managedForm:proceed")).click()
                     } else {
-                        available = true;
+                        available = true
                         for (int i = 0; i < 5; i++) {
-                            final Runnable runnable = (Runnable) Toolkit.getDefaultToolkit().
-                                    getDesktopProperty("win.sound.exclamation");
-                            if (runnable != null) runnable.run();
-                            Thread.sleep(500);
+                            FileInputStream fis = new FileInputStream(getClass().getResource("windows_11.mp3").getFile())
+                            Player playMP3 = new Player(fis)
+                            playMP3.play()
+                            Thread.sleep(500)
                         }
                     }
                 }
@@ -63,6 +62,7 @@ class TerminChecker {
                 CachingDriverFactory.clearCacheAndQuitDriver()
             }
             println("Exception was Thrown, starting over")
+            //todo: remove Recursion
             checkTermin()
         }
     }
